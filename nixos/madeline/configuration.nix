4526ead8a503fc306b3680bc32ae2b7c22ca5432
Madeline <madelinebarnes0706@gmail.com>
2025-04-17 15:55:21 -0700

    hyprland test

--- nixos/madeline/configuration.nix
+++ nixos/madeline/configuration.nix
@@ -5,6 +5,13 @@
 { config, pkgs, ... }:
 
 {
+# hyprland
+
+ nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
@@ -61,6 +68,10 @@
   environment.systemPackages = with pkgs; [
      neofetch
      git
+     hyprland
+     wayland
+     waybar
+     foot
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
